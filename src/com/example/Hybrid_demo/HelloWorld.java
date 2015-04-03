786fd8c14619b901d0253146fe3b6fe45d32a60c
jiangcheng.wxd <jiangcheng.wxd@taobao.com>
2015-04-03 20:22:20 +0800

    add camera method

--- src/com/example/Hybrid_demo/HelloWorld.java
+++ src/com/example/Hybrid_demo/HelloWorld.java
@@ -2,6 +2,7 @@ package com.example.Hybrid_demo;
 
 import android.content.Context;
 import android.widget.Toast;
+import android.graphics.Camera;
 
 /**
  * Created by admin on 15-4-3.
@@ -9,6 +10,8 @@ import android.widget.Toast;
 public class HelloWorld {
 
     Context mContext;
+
+    private Camera mCamera;
 
     /** Instantiate the interface and set the context */
     HelloWorld(Context c) {
@@ -19,5 +22,20 @@ public class HelloWorld {
     //@JavascriptInterface
     public void showToast(String toast) {
         Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+    }
+
+    public void openCamera(){
+        getCameraInstance();
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+//            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 }
